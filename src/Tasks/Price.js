8da1637f4ce36ff27eaa29b2596d37672a7344d7
NMuradyan <nar.muradian@gmail.com>
2020-11-02 20:49:08 +0400

    before pause

--- src/Tasks/Price.js
+++ src/Tasks/Price.js
@@ -1,9 +1,40 @@
 import React from "react";
 
 class Price extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            price: props.price,
+            rate: props.rate
+        };
+    }
+
+    buttonClick = ()=>{
+        let {price, rate} = this.state;
+        let sign = price[price.length-1];
+        if(sign === "$"){
+            let amd = parseFloat(price) * rate + "€";
+            this.setState({
+                price: amd
+            });
+        }else if(sign === "€"){
+            let usd = parseFloat(price) / rate + "$";
+            this.setState({
+                price: usd
+            });
+        }
+    };
 
     render() {
-        return <p>{this.props.price}</p>;
+        return (
+        <>
+        <p>Price: {this.state.price}</p>
+        <button onClick={this.buttonClick}>
+            Exchange
+        </button>
+        </>
+        );
     }
 }
 
